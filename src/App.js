80396e667fe92754aa03c06434c73bae08414390
Andrii Darmits <darmirs99@gmail.com>
2021-12-22 14:19:18 +0200

    created react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const users = ["Andrii", "Oleh", "Kiril", "Stepan"]
+
+
+
+function createList(users) {
+  if (users.length) {
+    return users.map(user => <li className="list">Активний користувач <span> {user}</span></li>);
+  } return <h2>There are no users</h2>
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My first react project</h1>
+      <ul>
+        {createList(users)}
+      </ul >
     </div>
+
+
   );
 }
 
