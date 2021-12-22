e76801c6d0287bbeacc2faeff85aa505b25a76e9
Andrii Darmits <darmirs99@gmail.com>
2021-12-22 21:01:16 +0200

    added list, some function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
-const users = ["Andrii", "Oleh", "Kiril", "Stepan"]
 
 
+
+const users = ["Andrii", "Oleh", "Kiril", "Stepan"];
 
 function createList(users) {
+
   if (users.length) {
-    return users.map(user => <li className="list">Активний користувач <span> {user}</span></li>);
-  } return <h2>There are no users</h2>
+    return (
+      <div>
+
+        <h3>Список активних юзерів</h3>
+        <ul>
+          {users.map(user => <li className="list">Активний користувач <span> {user}</span></li>)}
+        </ul>
+      </div>
+    );
+  } return <p>There are no users</p>
 }
+
+
+function checkUserAmount(users) {
+  if (users.length) {
+    return (
+      <div>
+        <p>Кількість активних юзерів ({users.length})</p>
+      </div>
+    )
+  }
+  return <p>There are no active users.</p>
+}
+
+
+const obj = {
+  firstName: 'Andrii',
+  lastName: 'Darmits',
+
+  getFullName: function () {
+    return (
+      <p>User name: {this.firstName} {this.lastName}</p>)
+  }
+}
+
+
 
 
 function App() {
   return (
     <div>
-      <h1>My first react project</h1>
-      <ul>
+      <h1 className="header">My first react application</h1>
+      {obj.getFullName()}
+      <div className="flex">
+        {checkUserAmount(users)}
         {createList(users)}
-      </ul >
+      </div>
     </div>
 
 
